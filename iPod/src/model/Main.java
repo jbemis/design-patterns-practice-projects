92b3cc754bb7792015f49e259c6b481e40335bb9
Joel Bemis <joel.bemis@gmail.com>
2018-08-20 16:02:56 +0300

    Refactored ipod
    iteration

--- iPod/src/model/Main.java
+++ iPod/src/model/Main.java
@@ -11,14 +11,14 @@ public class Main {
     private static Song s1 = new Song("Writing's on the Wall", "Sam Smith", "Ballad", 3.02);
     private static Photo p1 = new Photo("Moonrise", "Ansel Adams", "Landscape", "Yosemite");
 
-    // TODO: add more examples of media files you might find on an iPod below
 
     public static void main(String[] args) {
 
-       // TODO: add the static objects you've declared before this method to the iPod (nano)
+        nano.addMovie(m1);
+        nano.addSong(s1);
+        nano.addPhoto(p1);
 
         System.out.println(nano.getName() +"'s iPod contains: ");
-        // TODO: Use the Iterator Design Pattern to make the following for-each loop run
         for (AbstractMedia m : nano) {
             System.out.println(m.getName() + " by: " + m.getCreator());
         }
--- iPod/src/model/iPod.java
+++ iPod/src/model/iPod.java
@@ -1,10 +1,23 @@
 package model;
 
-public class iPod {
+import model.media.AbstractMedia;
+import model.media.Movie;
+import model.media.Photo;
+import model.media.Song;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.function.Consumer;
+
+public class iPod implements Iterable<AbstractMedia> {
 
     private String name;
+    private Set<AbstractMedia> movies = new HashSet<>();
+    private Set<AbstractMedia> photos = new HashSet<>();
+    private Collection<AbstractMedia> songs = new HashSet<>();
 
-    // TODO: add fields here which represent this iPod's Movies, Photos, and Songs, they should be of the Collection type
 
     public iPod(String name) {
         this.name = name;
@@ -13,6 +26,39 @@ public class iPod {
     // getters
     public String getName() { return name; }
 
+    public void addMovie(Movie m) { movies.add(m); }
+    public void addPhoto(Photo p) { photos.add(p); }
+    public void addSong(Song s) { songs.add(s); }
+
+    @Override
+    public Iterator<AbstractMedia> iterator() {
+        return new AbstractMediaIterator();
+    }
 
 
+    private class AbstractMediaIterator implements Iterator<AbstractMedia> {
+        Iterator moviesIterator = movies.iterator();
+        Iterator songsIterator = songs.iterator();
+        Iterator photosIterator = photos.iterator();
+
+        @Override
+        public boolean hasNext() {
+            return moviesIterator.hasNext() || songsIterator.hasNext() || photosIterator.hasNext();
+        }
+
+        @Override
+        public AbstractMedia next() {
+            AbstractMedia abstractMedia = null;
+            if (moviesIterator.hasNext()){
+                abstractMedia = (AbstractMedia) moviesIterator.next();
+            } else if (songsIterator.hasNext()){
+                abstractMedia = (AbstractMedia) songsIterator.next();
+            } else {
+                abstractMedia = (AbstractMedia) photosIterator.next();
+            }
+            return abstractMedia;
+        }
+
+
+    }
 }
